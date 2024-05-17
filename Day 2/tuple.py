a56ae353ea34f7b9ec14e2debbe3e02766caaaa4
Ganga Ghosh <122436755+gangaghosh@users.noreply.github.com>
2024-05-17 14:15:46 +0530

    Update tuple.py

--- Day 2/tuple.py
+++ Day 2/tuple.py
@@ -1,5 +1,23 @@
-# Convert a tuple to a list
-
+# 1. Convert a tuple to a list
 tuple = (1, 2, 3, 4, 5)
 my_list = list(tuple)
 print(my_list)
+
+
+# 2. Create a tuple and access its elements.
+my_tuple = (1, 2, 3, 4, 5)
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
+
+
+# 3. Concatenate two tuples
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+concatenated_tuple = tuple1 + tuple2
+print("Concatenated tuple:", concatenated_tuple)
+
+
+# 4. Find the length of a tuple
+ftuple = (10, 20, 30, 40, 50)
+length = len(ftuple)
+print("Length of the tuple:", length)
