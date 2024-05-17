331df2d1e2a3d1b460e9ae31d2197b82e02ab939
Ganga Ghosh <122436755+gangaghosh@users.noreply.github.com>
2024-05-17 14:14:39 +0530

    Update dictionary.py

--- Day 2/dictionary.py
+++ Day 2/dictionary.py
@@ -1,5 +1,26 @@
-# Iterate over keys and values in a dictionary.
-
+# 1. Iterate over keys and values in a dictionary.
 my_dict = {'name:':'Alice','age:': 25,'city:':'New York'}
 for i in my_dict:
     print(i,my_dict[i])
+
+
+# 2. Create a dictionary and access its elements
+dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+print("Name:", dict['name'])
+print("Age:", dict['age'])
+print("City:", dict['city'])
+
+
+# 3. Add a key-value pair to an existing dictionary.
+edict = {'name': 'Alice', 'age': 30}
+edict['city'] = 'New York'
+print("Updated dictionary:", edict)
+
+
+# 4. Check if a key exists in a dictionary
+kdict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+key_to_check = 'city'
+if key_to_check in kdict:
+    print(f"{key_to_check} exists in the dictionary.")
+else:
+    print(f"{key_to_check} does not exist in the dictionary.")
